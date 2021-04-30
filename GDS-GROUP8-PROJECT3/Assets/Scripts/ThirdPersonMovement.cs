def3085223f088658fd57225930da704659f96e1
Vesorad <57622258+Vesorad@users.noreply.github.com>
2021-04-30 21:14:57 +0200

    Dodanie Paczek do glównego projektu
    -TMpro
    -ProGrid
    -ProBuilder

--- GDS-GROUP8-PROJECT3/Assets/Scripts/ThirdPersonMovement.cs
+++ GDS-GROUP8-PROJECT3/Assets/Scripts/ThirdPersonMovement.cs
@@ -8,6 +8,7 @@ public class ThirdPersonMovement : MonoBehaviour
 
     public float speed = 6f;
     public float turnSmoothTime = 0.1f;
+    public Transform cam;
 
     float turnSmoothVelocity;
 
@@ -19,7 +20,7 @@ public class ThirdPersonMovement : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
 
