a20d3d8bb690cf96b0ade8c222ea948037ef97ce
Vesorad <57622258+Vesorad@users.noreply.github.com>
2021-05-09 10:11:11 +0200

    poruszanie się v1.0

--- GDS-GROUP8-PROJECT3/Assets/Scripts/FirstPersonMovement.cs
+++ GDS-GROUP8-PROJECT3/Assets/Scripts/FirstPersonMovement.cs
@@ -9,22 +9,24 @@ public class FirstPersonMovement : MonoBehaviour
 {
     [SerializeField] private Transform playerBody;
     [SerializeField] private float mouseSen =100f;
-
     private float _xRotation = 0;
 
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
     }
-
     void Update()
+    {
+        MovementCamera();
+    }
+    private void MovementCamera()
     {
         var mouseX = Input.GetAxis("Mouse X") * mouseSen* Time.deltaTime;
         var mouseY = Input.GetAxis("Mouse Y")* mouseSen * Time.deltaTime;
 
         _xRotation -= mouseY;
         _xRotation = Mathf.Clamp(_xRotation, -90, 90);
-        transform.localRotation = Quaternion.Euler(_xRotation,0f,0f);
+       // transform.localRotation = Quaternion.Euler(_xRotation,0f,0f);
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
--- GDS-GROUP8-PROJECT3/Assets/Scripts/PlayerMovement.cs
+++ GDS-GROUP8-PROJECT3/Assets/Scripts/PlayerMovement.cs
@@ -4,24 +4,37 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    [Header("Main")]
     [SerializeField] private CharacterController characterController;
-
     [SerializeField] private float speed = 12;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] private float gravity = -9.81f;
+    [Header("Ground Check")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundDistance = 0.4f;
+    [SerializeField] private LayerMask groundMask;
+    
+    private Vector3 _velocity;
+    private bool _isGrounded;
     void Update()
+    {
+        FallCheck();
+        MainMovement();
+    }
+    private void FallCheck()
+    {
+        _isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (_isGrounded && _velocity.y<0)
+        {
+            _velocity.y = -2f;
+        }
+    }
+    private void MainMovement()
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-
         Vector3 move = transform.right * x + transform.forward * z;
-
         characterController.Move(move * speed* Time.deltaTime);
+        _velocity.y += gravity * Time.deltaTime;
+        characterController.Move(_velocity * Time.deltaTime); 
     }
 }
