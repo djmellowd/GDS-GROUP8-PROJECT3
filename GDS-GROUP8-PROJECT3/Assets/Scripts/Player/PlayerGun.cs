26422b5617b9f5f48adacfd5c18658f04d5f4ab4
Vesorad <57622258+Vesorad@users.noreply.github.com>
2021-05-12 11:31:28 +0200

    -Dodanie strzelnia gracza
    -skrypt target
    -skrypt gun

---
+++ GDS-GROUP8-PROJECT3/Assets/Scripts/Player/PlayerGun.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGun : MonoBehaviour
+{
+    [Header("Basic Parameters")]
+    [SerializeField] private float damage = 10f;
+    [SerializeField] private float range = 100f;
+    
+    [Header("Objects")]
+    [SerializeField] private Camera camera;
+
+    [SerializeField] private ParticleSystem muzzleFlash;
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        muzzleFlash.Play();
+        
+        RaycastHit hitInfo;
+        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hitInfo, range))
+        {
+          Target target = hitInfo.transform.GetComponent<Target>();
+          if (target != null)
+          {
+             target.TakeDamage(damage); 
+          }
+        }
+        
+    }
+}
