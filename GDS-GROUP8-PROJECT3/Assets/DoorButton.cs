4043f6e4b84eb35c55f37ba4f620b1290f5da9c3
Vesorad <57622258+Vesorad@users.noreply.github.com>
2021-06-16 09:24:25 +0200

    Zrobienie dzwi na trigger
    -Dopiracowanie drzwi na przycisk

---
+++ GDS-GROUP8-PROJECT3/Assets/DoorButton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorButton : MonoBehaviour
+{
+    [SerializeField]private Renderer mRenderer;
+    public bool openDoor;
+    
+   
+
+    private void OnMouseOver()
+    {
+        Debug.Log(1);
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Vector3 newPos =  new Vector3(transform.position.x - 0.2f,transform.position.y,transform.position.z);
+            gameObject.transform.Translate(newPos);
+            openDoor = true;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        Debug.Log(0);
+    }
+}
--- GDS-GROUP8-PROJECT3/Assets/OurScripts/AutomaticDoor.cs
+++ GDS-GROUP8-PROJECT3/Assets/OurScripts/AutomaticDoor.cs
@@ -10,6 +10,12 @@ public class AutomaticDoor : MonoBehaviour
     [SerializeField] private float deviation=0;
     [SerializeField] private int speed;
 
+    [Header("Otwieranie")] 
+    [SerializeField] private bool onTrigger;
+    [SerializeField] private bool OnButton;
+    [SerializeField] private DoorButton button;
+
+    private bool _closeDoor;
     private Vector3 _leftDir;
     private Vector3 _rightDir;
     private Vector3 _leftStart;
@@ -23,21 +29,54 @@ public class AutomaticDoor : MonoBehaviour
         _rightDir = new Vector3(rightPart.position.x+deviation,rightPart.position.y,rightPart.position.z);
     }
 
-    void Update()
+    private void OnTriggerStay(Collider other)
     {
-        if (Input.GetKey(KeyCode.Q))
+        if (onTrigger)
         {
-            leftPart.position = Vector3.MoveTowards( leftPart.position,_leftDir,speed*Time.deltaTime);
-            rightPart.position = Vector3.MoveTowards(rightPart.position,_rightDir,speed*Time.deltaTime);
+            if (other.tag == "Player")
+            {
+                _closeDoor = false;
+                OpenDoor();
+            }  
         }
-        if (Input.GetKey(KeyCode.E))
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (onTrigger)
         {
-            Vector3 currentPosLeft = leftPart.position;
-            Vector3 currentPosRight = rightPart.position;
-                leftPart.position = Vector3.MoveTowards(currentPosLeft,_leftStart,speed*Time.deltaTime);
-                rightPart.position = Vector3.MoveTowards(currentPosRight,_rightStart,speed*Time.deltaTime);
-            
-           
+            if (other.tag == "Player")
+            {
+                _closeDoor = true;
+            } 
         }
+    }
+
+    private void Update()
+    {
+        if (OnButton)
+        {
+            if (button.openDoor)
+            {
+                OpenDoor();
+            } 
+        }
+        if (_closeDoor)
+        {
+           CloseDoor();
+        }
+    }
+
+    private void OpenDoor()
+    {
+        leftPart.position = Vector3.MoveTowards( leftPart.position,_leftDir,speed*Time.deltaTime);
+        rightPart.position = Vector3.MoveTowards(rightPart.position,_rightDir,speed*Time.deltaTime);
+    }
+
+    private void CloseDoor()
+    {
+        Vector3 currentPosLeft = leftPart.position;
+        Vector3 currentPosRight = rightPart.position;
+        leftPart.position = Vector3.MoveTowards(currentPosLeft,_leftStart,speed*Time.deltaTime);
+        rightPart.position = Vector3.MoveTowards(currentPosRight,_rightStart,speed*Time.deltaTime);
     }
 }
