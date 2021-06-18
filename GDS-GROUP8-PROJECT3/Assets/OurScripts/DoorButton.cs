70c2ae6541792dbd4e85ef035e433faa615a14cd
Vesorad <57622258+Vesorad@users.noreply.github.com>
2021-06-18 23:36:51 +0200

    save

--- GDS-GROUP8-PROJECT3/Assets/OurScripts/DoorButton.cs
+++ GDS-GROUP8-PROJECT3/Assets/OurScripts/DoorButton.cs
@@ -6,23 +6,47 @@ using UnityEngine;
 public class DoorButton : MonoBehaviour
 {
     [SerializeField]private Renderer mRenderer;
+    [SerializeField]private bool onHit;
+    [SerializeField]private int hit = 3;
+    [SerializeField]private bool onClick;
     public bool openDoor;
-    
-   
+
+    private int _hitTimes;
 
     private void OnMouseOver()
     {
-        Debug.Log(1);
-        if (Input.GetKeyDown(KeyCode.E))
+        if (onClick)
         {
-            Vector3 newPos =  new Vector3(transform.position.x - 0.2f,transform.position.y,transform.position.z);
-            gameObject.transform.Translate(newPos);
-            openDoor = true;
+            mRenderer.material.color = Color.gray;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Vector3 newPos =  new Vector3(transform.position.x - 0.2f,transform.position.y,transform.position.z);
+                gameObject.transform.Translate(newPos);
+                openDoor = true;
+            }
+        }
+       
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Bullet")
+        {
+            Debug.Log(1);
+            _hitTimes += 1;
+            if (_hitTimes== hit)
+            {
+                openDoor = true;
+            }
         }
     }
 
     private void OnMouseExit()
     {
-        Debug.Log(0);
+        if (onClick)
+        {
+            mRenderer.material.color = Color.black;
+        }
+        
     }
 }
