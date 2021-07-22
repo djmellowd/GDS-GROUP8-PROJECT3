b3660f114243513ecf915c4b109152d397b7e379
Vesorad <57622258+Vesorad@users.noreply.github.com>
2021-07-23 00:16:05 +0200

    Wszystko w tasku pod ta samą nazwa

---
+++ GDS-GROUP8-PROJECT3/Assets/Scripts/Singleton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton : MonoBehaviour
+{
+    public static Singleton _Singleton;
+    void Awake()
+    {
+        if (_Singleton == null)
+            _Singleton = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+       DontDestroyOnLoad(gameObject);
+    }
+}
