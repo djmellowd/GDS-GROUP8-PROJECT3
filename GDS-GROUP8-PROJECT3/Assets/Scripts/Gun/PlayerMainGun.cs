371275198fa9bdb33d48dcc37c3194e8ca6bea10
Vesorad <57622258+Vesorad@users.noreply.github.com>
2021-07-28 20:25:04 +0200

    Update PlayerMainGun.cs

--- GDS-GROUP8-PROJECT3/Assets/Scripts/Gun/PlayerMainGun.cs
+++ GDS-GROUP8-PROJECT3/Assets/Scripts/Gun/PlayerMainGun.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class PlayerMainGun : MonoBehaviour
 {
+    private const string OVERHEAT_STRING = "Overheat";
+
     [SerializeField]
     private Transform spawnPoint;
     [SerializeField]
@@ -19,20 +21,26 @@ public class PlayerMainGun : MonoBehaviour
     private GameObject ammoPreFab;
     [SerializeField]
     private GameObject muzzlePrefab;
+    [SerializeField]
+    private HudManager hudManager;
 
     private List<GameObject> ammoList = new List<GameObject>();
-    private float timeToFire;
+    
     private Vector3 destination;
     private int resetGun;
+    private bool playAudioOverheat = false;
+    private float timeToFire;
     private float normalGunFov;
     private float smoothFovTransition;
-
+    private AudioManager audioManager;
     void Awake()
     {
+        audioManager = FindObjectOfType<AudioManager>();
         normalGunFov = mainCamera.fieldOfView;
 
         PullObjectAmmo();
     }
+
     void Update()
     {
         Overheating();
@@ -66,21 +74,26 @@ public class PlayerMainGun : MonoBehaviour
     }
     private void Overheating()
     {
+        if (resetGun >= bullet.limitAmmo)
+        {
+            gunFragmentRender.materials[0].color = Color.red;
+            if (!playAudioOverheat)
+            {
+                audioManager.Play(OVERHEAT_STRING);
+                playAudioOverheat = true;
+            }
+
+            return;
+        }
         if (Input.GetMouseButton(0) && Time.time >= timeToFire)
         {
             timeToFire = Time.time + 1 / bullet.fireRate;
+            StopAllCoroutines();
+            resetGun += 1;
+            hudManager.RefreshOverheatPlayer(resetGun);
+            Shooting();
+            StartCoroutine(TimeBetweenShoots());
 
-            if (resetGun < bullet.limitAmmo)
-            {
-                StopAllCoroutines();
-                resetGun += 1;
-                Shooting();
-                StartCoroutine(TimeBetweenShoots());
-            }
-            else
-            {
-                gunFragmentRender.materials[0].color = Color.red;
-            }
         }
     }
 
@@ -89,7 +102,9 @@ public class PlayerMainGun : MonoBehaviour
     {
         yield return new WaitForSeconds(bullet.overheatingTime);
         gunFragmentRender.materials[0].color = Color.yellow;
+        playAudioOverheat = false;
         resetGun = 0;
+        hudManager.RefreshOverheatPlayer(0);
     }
 
     private void Shooting()
@@ -117,5 +132,6 @@ public class PlayerMainGun : MonoBehaviour
             }
         }
     }
+
 
 }
