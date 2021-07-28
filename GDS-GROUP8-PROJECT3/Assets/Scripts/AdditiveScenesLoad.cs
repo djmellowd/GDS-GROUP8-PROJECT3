9a9581af45b6ea760052537f8e63bdbd946a3a28
Pawel Milczanowski <pawel.milczanowski@outlook.com>
2021-07-28 13:45:10 +0200

    New additive scene added to the script

--- GDS-GROUP8-PROJECT3/Assets/Scripts/AdditiveScenesLoad.cs
+++ GDS-GROUP8-PROJECT3/Assets/Scripts/AdditiveScenesLoad.cs
@@ -4,8 +4,10 @@ using UnityEngine.SceneManagement;
 public class AdditiveScenesLoad : MonoBehaviour
 {
     public string sceneName;
+    public string sceneName2;
     void Awake()
     {
         UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName2, LoadSceneMode.Additive);
     }
 }
