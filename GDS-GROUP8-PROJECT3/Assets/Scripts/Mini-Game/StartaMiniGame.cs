3cc8ea2c65276f02100d32f823948f31c7fef39f
Pawel Milczanowski <pawel.milczanowski@outlook.com>
2021-08-03 18:41:15 +0200

    Mini-Game Implementation

---
+++ GDS-GROUP8-PROJECT3/Assets/Scripts/Mini-Game/StartaMiniGame.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartaMiniGame : MonoBehaviour
+{
+    [SerializeField] private Renderer mRenderer;
+    public bool openDoor;
+    [SerializeField] private bool mainDoor;
+    [SerializeField] private List<Renderer> parts;
+    [SerializeField] private Objects seriaObject;
+
+    private AudioManager audioManager;
+    private int rangeToClick;
+
+    public string sceneName4;
+
+    private void Awake()
+    {
+        rangeToClick = seriaObject.rangeToClick;
+    }
+    private void OnMouseOver()
+    {
+        if (Vector3.Distance(GameObject.FindWithTag("Player").transform.position, transform.position) > rangeToClick)
+        {
+            mRenderer.material.color = Color.black;
+            return;
+        }
+
+        mRenderer.material.color = Color.gray;
+        if (Input.GetKeyDown(seriaObject.button))
+        {
+            if (!openDoor)
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName4, LoadSceneMode.Additive);
+                openDoor = true;
+                if (mainDoor)
+                {
+                    foreach (var VARIABLE in parts)
+                    {
+                        VARIABLE.material.color = Color.green;
+                    }
+                }
+            }
+            else
+            {
+                openDoor = false;
+            }
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        mRenderer.material.color = Color.black;
+    }
+}
---
+++ GDS-GROUP8-PROJECT3/Assets/Scripts/Mini-Game/destroy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class destroy : MonoBehaviour
+{
+
+    public string sceneName5;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync(sceneName5);
+    }
+}
