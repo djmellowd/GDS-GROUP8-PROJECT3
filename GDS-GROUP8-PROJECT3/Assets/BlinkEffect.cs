53df39b920e58c2693a3421ac16acccd78300aac
Vesorad <57622258+Vesorad@users.noreply.github.com>
2021-08-03 21:19:14 +0200

    -Add Lamp

---
+++ GDS-GROUP8-PROJECT3/Assets/BlinkEffect.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkEffect : MonoBehaviour
+{
+
+    [SerializeField] private Light light;
+    [SerializeField] private ScritableOneValue blinkSpeedValue;
+    [SerializeField] private ScritableOneValue timeOfValue;
+
+    private float blinkValue;
+
+    private float noramlIntensity;
+    private void Awake()
+    {
+        blinkValue = blinkSpeedValue.Value;
+        noramlIntensity = light.intensity;
+    }
+
+    void Start()
+    {
+        InvokeRepeating("Blink", 0, blinkValue);
+    }
+
+   
+    private void Blink()
+    {
+        StartCoroutine(BlinkStart());
+    }
+    IEnumerator BlinkStart()
+    {
+        light.intensity = 0;
+        yield return new WaitForSeconds(timeOfValue.Value);
+        light.intensity = noramlIntensity;
+    }
+}
