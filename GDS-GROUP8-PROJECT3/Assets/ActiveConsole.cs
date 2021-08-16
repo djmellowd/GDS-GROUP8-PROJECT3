beca86125af7d4ab3f65449734ccb3f3bd55ef11
Vesorad <57622258+Vesorad@users.noreply.github.com>
2021-08-16 18:16:02 +0200

    Save test Scene

---
+++ GDS-GROUP8-PROJECT3/Assets/ActiveConsole.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveConsole : MonoBehaviour
+{
+    [SerializeField] private Transform cameraPoint;
+    [SerializeField] private Objects seriaObject;
+
+    [SerializeField] private float speed;
+    private GameContoller gameContoller;
+
+
+    private Camera playerCam;
+    private Transform startPosCamera = null;
+    private bool isOnConsole=false;
+
+    private void Awake()
+    {    
+        gameContoller = FindObjectOfType<GameContoller>();
+        playerCam = gameContoller.MainCamera;
+    }
+    private void OnMouseOver()
+    {
+        if (isOnConsole)
+        {
+            return;
+        }
+        if (Vector3.Distance(GameObject.FindWithTag("Player").transform.position, transform.position) > seriaObject.rangeToClick)
+        {
+            return;                    
+        }
+        if (Input.GetKeyDown(seriaObject.button))
+        {
+            startPosCamera = playerCam.transform;
+            DisActiveGame();
+        }
+    }
+
+    private void DisActiveGame()
+    {
+        isOnConsole = true;
+
+        gameContoller.MainCanvas.gameObject.SetActive(false);
+        gameContoller.Player.SetActive(false);
+        gameContoller.PlayerGun.SetActive(false);
+    }
+    private void ActiveGame()
+    {
+        isOnConsole = false;
+        gameContoller.MiniGameCanvas.gameObject.SetActive(false);
+        gameContoller.MainCanvas.gameObject.SetActive(true);
+        gameContoller.Player.SetActive(true);
+        gameContoller.PlayerGun.SetActive(true);
+
+        playerCam.transform.parent = gameContoller.HeadPlayer.transform;
+        playerCam.transform.position = gameContoller.HeadPlayer.transform.position;
+        playerCam.transform.rotation = gameContoller.HeadPlayer.transform.rotation;
+    }
+
+    private void Update()
+    {
+        if (isOnConsole && playerCam.transform.position != cameraPoint.position && playerCam.transform.rotation != cameraPoint.rotation)
+        {
+            playerCam.transform.parent = cameraPoint;
+            playerCam.transform.position = Vector3.Lerp(playerCam.transform.position, cameraPoint.position, speed * Time.deltaTime);
+            playerCam.transform.rotation = Quaternion.Lerp(playerCam.transform.rotation, cameraPoint.rotation, speed * Time.deltaTime);
+        }
+        else if (isOnConsole)
+        {
+            gameContoller.MiniGameCanvas.gameObject.SetActive(true);
+            if (Input.GetKeyDown(seriaObject.button))
+            {
+                ActiveGame();
+            }
+        }
+    }
+}
---
+++ GDS-GROUP8-PROJECT3/Assets/MiniGame_GameController.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGame_GameController : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private Transform[] checkPoints;
+    [SerializeField] private float speedPlayer;
+    [SerializeField] private LineRenderer[] lineRenderer;
+
+    private int currentCheckPoint = 0;
+    Transform currentPoint;
+    Transform nextPoint;
+
+    private void Awake()
+    {     
+        currentPoint = checkPoints[currentCheckPoint];
+        nextPoint = checkPoints[currentCheckPoint + 1];
+        StartGame();
+    }
+
+    void Update()
+    {
+        CheckNextPointLocation();
+
+    }
+
+    private void StartGame()
+    {
+        Debug.Log("GAME START!");
+        currentCheckPoint = 0;
+        RefreshPoints();
+        player.transform.position = checkPoints[0].position;
+
+        for (int i = 0; i < checkPoints.Length; i++)
+        {
+            lineRenderer[i].positionCount = 1;
+            lineRenderer[i].SetPosition(0, checkPoints[i].position);
+        }
+        
+    }
+
+    private void GoToNextPoint()
+    {
+       
+        currentCheckPoint++;
+        
+        player.transform.position = nextPoint.position;
+        SetUpLine();
+        if (checkPoints.Length <= currentCheckPoint+1)
+        {
+            currentPoint = checkPoints[currentCheckPoint];
+            Debug.Log("YOU WIN!");
+        }
+        else
+        {
+            RefreshPoints();
+        }
+    }
+
+    private void RefreshPoints()
+    {      
+        currentPoint = checkPoints[currentCheckPoint];
+        nextPoint = checkPoints[currentCheckPoint + 1];
+    }
+
+    public void SetUpLine()
+    {
+        lineRenderer[currentCheckPoint-1].positionCount++;
+        lineRenderer[currentCheckPoint-1].SetPosition(1, player.transform.position);
+    }
+
+    private void CheckNextPointLocation()
+    {
+
+ /*      if (player.transform.position != currentPoint.position)
+        {
+            player.transform.position = Vector3.Lerp(player.transform.position, currentPoint.position, speedPlayer * Time.deltaTime);
+            return;
+        }
+ */
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (Mathf.Round(currentPoint.position.x) == Mathf.Round(nextPoint.position.x) && currentPoint.position.y < nextPoint.position.y)
+            {
+                GoToNextPoint();
+            }
+            else
+            {
+                StartGame();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (Mathf.Round(currentPoint.position.x) == Mathf.Round(nextPoint.position.x) && currentPoint.position.y > nextPoint.position.y)
+            {
+                GoToNextPoint();
+            }
+            else
+            {
+                StartGame();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (Mathf.Round(currentPoint.position.x) == Mathf.Round(nextPoint.position.x) && currentPoint.position.x < nextPoint.position.x)
+            {
+                GoToNextPoint();
+            }
+            else
+            {
+                StartGame();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (Mathf.Round(currentPoint.position.y) == Mathf.Round(nextPoint.position.y)&& currentPoint.position.x > nextPoint.position.x)
+            {
+                GoToNextPoint();
+            }
+            else
+            {
+                StartGame();
+            }
+        }       
+    }
+}
