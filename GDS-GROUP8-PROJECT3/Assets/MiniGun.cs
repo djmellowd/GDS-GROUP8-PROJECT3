34602ee806b3f015f66394e792ce01c3c70c969e
Vesorad <57622258+Vesorad@users.noreply.github.com>
2021-09-15 21:50:43 +0200

    save

--- GDS-GROUP8-PROJECT3/Assets/MiniGun.cs
+++ GDS-GROUP8-PROJECT3/Assets/MiniGun.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MiniGun : MonoBehaviour
 {
+    private const int RESET_TIMER = 0;
     [Header("Patrol")]
     [SerializeField] private int patrolRange;
     [SerializeField] private int patrolSpeed;
@@ -14,9 +15,11 @@ public class MiniGun : MonoBehaviour
     [SerializeField] private int rottateBarrelSpeed = 650;
 
     [Header("Shoot")]
-    [SerializeField] private GameObject ammo;
-    [SerializeField] private float speedAmmo;
+    [SerializeField] private GameObject ammoPrefab;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private float shootingFrequency;
     [SerializeField] private float RangeToSeePlayer;
+    [SerializeField] private float RedZone;
 
     private Quaternion leftRotation;
     private Quaternion rightRotation;
@@ -25,12 +28,16 @@ public class MiniGun : MonoBehaviour
     private GameObject player;
     private GameContoller gameContoller;
 
+    private List<GameObject> ammoList = new List<GameObject>();
+    private float timer;
+
     void Awake()
     {
         leftRotation = Quaternion.Euler(0, 180 + patrolRange, 0);
         rightRotation = Quaternion.Euler(0, 180 - patrolRange, 0);
 
         InvokeRepeating("SwtichRotate", 0, offSetPatrol);
+        ObjectPullAmmo();
     }
 
     private void Start()
@@ -41,26 +48,54 @@ public class MiniGun : MonoBehaviour
 
     void Update()
     {
-        if (Vector3.Distance(transform.position, player.transform.position) <= RangeToSeePlayer)
-        {          
-            Shoot();
+        var distance = Vector3.Distance(transform.position, player.transform.position);
+        if (distance >= RangeToSeePlayer || distance <= RedZone)
+        {
+            timer = RESET_TIMER;
+            Patrol();
         }
         else
-        {
-            Patrol();
+        {          
+            Shoot();
         }      
+    }
+
+    private void ObjectPullAmmo()
+    {
+        for (int i = 0; i < ammoPrefab.GetComponent<EnemyAmmoInGame>().EnemyBullet.LimitAmmo; i++)
+        {
+            GameObject ammo = Instantiate(ammoPrefab);
+            ammoList.Add(ammo);
+            ammo.SetActive(false);
+        }
     }
 
     private void Shoot()
     {
         LookAtPlayer();
         RotateBarrel();
+
+        timer += Time.deltaTime;
+        if (timer >= shootingFrequency)
+        {
+            timer = RESET_TIMER;
+            for (int i = 0; i < ammoList.Count; i++)
+            {
+                if (!ammoList[i].activeInHierarchy)
+                {
+                    ammoList[i].transform.position = spawnPoint.position;
+                    ammoList[i].transform.rotation = gameObject.transform.localRotation;
+                    ammoList[i].SetActive(true);
+                    return;
+                }
+            }
+        }          
     }
 
     private void LookAtPlayer()
     {
         var lookPos = player.transform.position - transform.position;
-        lookPos.y = 0;
+        lookPos.y += 1;
         var rotation = Quaternion.LookRotation(lookPos);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rottateBarrelSpeed);
         
--- GDS-GROUP8-PROJECT3/Assets/Scripts/ScriptableObjects/EnemyBullet.cs
+++ GDS-GROUP8-PROJECT3/Assets/Scripts/ScriptableObjects/EnemyBullet.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[CreateAssetMenu(fileName ="EnemyBullet",menuName ="ScriptableObjects/EnemyBullet", order = 1)] 
+[CreateAssetMenu(fileName = "EnemyBullet", menuName = "ScriptableObjects/EnemyBullet", order = 1)]
 public class EnemyBullet : ScriptableObject
 {
-   public float damage=20;
-   public float speed=5;
-   public float selfDestructTime =5;
+    public int LimitAmmo;
+    public float damage = 20;
+    public float speed = 5;
+    public float selfDestructTime = 5;
 }
