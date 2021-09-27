986eb0a4f7826c83476e26c269d13b5135f9ea00
Vesorad <57622258+Vesorad@users.noreply.github.com>
2021-09-27 22:10:18 +0200

    Add TextBox

---
+++ GDS-GROUP8-PROJECT3/Assets/TextBox.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextBox : MonoBehaviour
+{
+    private const string PLAYER_TAG = "Player";
+
+    [SerializeField] [TextArea(1, 4)] private string textInBox;
+    [SerializeField] private float timeToHide;
+
+    [HideInInspector] public TextBoxManager TextBoxManager;
+
+    private bool firstAwake = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!firstAwake)
+        {
+            if (other.gameObject.tag == PLAYER_TAG)
+            {
+                firstAwake = true;
+                TextBoxManager.ActiveBox(textInBox, timeToHide);
+            }
+        }    
+    }
+}
---
+++ GDS-GROUP8-PROJECT3/Assets/TextBoxManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextBoxManager : MonoBehaviour
+{
+    [SerializeField] private GameObject boxCanvas;
+    [SerializeField] private TextMeshProUGUI textInBox;
+    [SerializeField] private List<TextBox> texts;
+    private void Awake()
+    {
+        foreach (var item in texts)
+        {
+            item.TextBoxManager = this;
+        }
+    }
+    public void ActiveBox(string text, float timeToHide)
+    {
+        textInBox.text = text;
+        boxCanvas.SetActive(true);
+        StartCoroutine(WaitForHideBox(timeToHide));
+    }
+
+    IEnumerator WaitForHideBox(float time)
+    {
+        yield return new WaitForSeconds(time);
+        HideBox();
+    }
+    private void HideBox()
+    {
+        boxCanvas.SetActive(false);
+    }
+}
