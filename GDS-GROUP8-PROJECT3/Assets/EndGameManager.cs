bc537f590c86eefc312e431606f58da4bce973a0
Vesorad <57622258+Vesorad@users.noreply.github.com>
2021-10-04 22:27:01 +0200

    Add end game

---
+++ GDS-GROUP8-PROJECT3/Assets/EndGameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameManager : MonoBehaviour
+{
+    [SerializeField] private List<EndGameButton> endGameButtons;
+
+    private bool firstAwake=true;
+
+    private void Update()
+    {
+        if (firstAwake)
+        {
+            if (IsAllMissionComplete())
+            {
+                Debug.Log("KONIEC GRY");
+                firstAwake = false;
+            }
+        }
+        
+    }
+
+    private bool IsAllMissionComplete()
+    {
+        for (int i = 0; i < endGameButtons.Count; ++i)
+        {
+            if (endGameButtons[i].IsActive == false)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
