913ea3252cb3ce03468297cbfc3a8fb2cf9565fc
Vesorad <57622258+Vesorad@users.noreply.github.com>
2021-10-06 23:17:04 +0200

    Add Blink Light

---
+++ GDS-GROUP8-PROJECT3/Assets/BlinkLight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkLight : MonoBehaviour
+{
+    [SerializeField] private Light light;
+    [SerializeField] private float timeBetweenBlink;
+
+    private float awakeIntensity = 0;
+    private void Awake()
+    {
+        awakeIntensity = light.intensity;
+        Blink();
+    }
+
+    private void Blink()
+    {
+        InvokeRepeating("LightOff", timeBetweenBlink, timeBetweenBlink);
+        InvokeRepeating("LightOn", timeBetweenBlink + 0.1f, timeBetweenBlink + 0.1f);
+    }
+
+    private void LightOff()
+    {
+        light.intensity = 0;
+    }
+
+    private void LightOn()
+    {
+        light.intensity = awakeIntensity;
+    }
+}
