02a58f4104a3e0ba174f84849fb10a3fa279b4a9
Mateusz Kolasiński <57622258+Vesorad@users.noreply.github.com>
2021-10-14 21:55:13 +0200

    Add CutScene

---
+++ GDS-GROUP8-PROJECT3/Assets/EndGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    [SerializeField] private float time;
+
+    private void Awake()
+    {
+        StartCoroutine(EndCutScene());
+    }
+
+    IEnumerator EndCutScene()
+    {
+        yield return new WaitForSeconds(time);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenuNew");
+    }
+}
